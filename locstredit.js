1117f7291b18da56fdd61a171cd7e35f5d5e4cdb
x2605 <69812394+x2605@users.noreply.github.com>
2021-02-17 16:55:55 +0900

    localisation 한국어

--- locstredit.js
+++ locstredit.js
@@ -1,3 +1,60 @@
+lang = {};
+lang['^ko'] = {
+    title: '팩토리오 Locale 문자열 편집기',
+    69: '줄<br>바꿈',
+    70: '누락 부분<br>생성',
+    71: '차이 부분<br>생성',
+    72: '동일 부분<br>생성',
+    73: '줄 번호<br>표기',
+    74: '줄 번호<br>제거',
+    75: '키 이름<br>떼기',
+    76: '키 이름<br>붙이기',
+    77: '현지화<br>병합',
+    100: '원본',
+    101: '현지화',
+    102: '비교',
+    900: 'msg: 900\n* 키 이름 떼기 * 실패.\n"비교" 영역에 줄 번호가 붙어있지 않은 것 같습니다. "줄 번호 표기"를 먼저 실행하십시오.\n\n문제 부분:\n\n',
+    910: 'msg: 910\n* 키 이름 떼기 * 실패.\n"비교" 영역에 파싱할 것이 아무것도 없습니다.',
+    920: 'msg: 920\n* 키 이름 붙이기 * 실패.\n"비교" 영역의 무언가가 잘못되었습니다. "키 이름 떼기"를 먼저 성공한 것이 맞습니까? 그렇다면 각 줄의 접두문자열을 유지해야 합니다. 의도하지 않은 문자가 있는지 확인하십시오.\n\n문제 부분:\n\n',
+    930: 'msg: 930\n"비교" 영역의 텍스트가 변경되며 되돌릴 수 없습니다. 계속하시겠습니까?'
+};
+lang['^en'] = {
+    title: 'Factorio Locale String Editor',
+    69: 'word<br>wrap',
+    70: 'generate<br>missing',
+    71: 'generate<br>difference',
+    72: 'generate<br>identical',
+    73: 'tag line<br>number',
+    74: 'untag line<br>number',
+    75: 'detach<br>key names',
+    76: 'attach<br>key names',
+    77: 'merge with<br>localised',
+    100: 'original',
+    101: 'localised',
+    102: 'compare',
+    900: 'msg: 900\n* detach key names * FAILED.\nLine number seems not tagged at texts in "compare" section. Do "tag line number" first.\n\nProblem in:\n\n',
+    910: 'msg: 910\n* detach key names * FAILED.\nThere is nothing to be parsed in "compare" section.',
+    920: 'msg: 920\n* attach key names * FAILED.\nSomething wrong in "compare" section. Did you really do "detach key names" first successful? If so, preserve all prefixes of each line. Check for unintended characters.\n\nProblem in:\n\n',
+    930: 'msg: 930\nTexts in "compare" section will be changed irreversible. Are you sure to continue?'
+};
+dialmsg = Object.assign({}, lang['^en']);
+window.onload = function(){
+    var reg;
+    for(var key in lang){
+        reg = new RegExp(key, 'i');
+        if(navigator.language.match(reg)){
+            var doms = document.querySelectorAll('[data-langKey]');
+            for(var i = 0; i < doms.length; i++){
+                doms[i].innerHTML = lang[key][doms[i].dataset['langkey']]
+            };
+            dialmsg = Object.assign({}, lang[key]);
+            break;
+        }
+    }
+};
+
+/***************************************************************************/
+
 var editor_names = ['text_ori', 'text_loc', 'text_com'];
 
 function ew(e){
@@ -184,12 +241,12 @@ function detachkey(){try{
             }
         }
         else{
-            window.alert('* detach key names * FAILED.\nLine number seems not tagged at texts in "compare" section. Do "tag line number" first.\n\nProblem in:\n\n' + t);
+            window.alert(dialmsg[900] + t);
             return;
         }
     }
     if(!met_normal){
-        window.alert('* detach key names * FAILED.\nThere is nothing to be parsed in "compare" section.');
+        window.alert(dialmsg[910]);
         return;
     }
     if(m){
@@ -225,7 +282,7 @@ function attachkey(){try{
         var t = spl[i];
         m = t.match(reg);
         if(!m){
-            window.alert('* attach key names * FAILED.\nSomething wrong in "compare" section. Did you really do "detach key names" first successful? If so, preserve all prefixes of each line. Check for unintended characters.\n\nProblem in:\n\n' + t);
+            window.alert(dialmsg[920] + t);
             return;
         }
         if(parseInt(m[1]) > 0){
@@ -253,7 +310,7 @@ function mergecom2loc(){try{
     var ori = parse(document.getElementById('text_ori').value);
     var loc = parse(document.getElementById('text_loc').value, ori);
     var com = parse(document.getElementById('text_com').value, ori);
-    if(window.confirm('Texts in "compare" section will be changed irreversible. Are you sure to continue?')){
+    if(window.confirm(dialmsg[930])){
         var newstr = '';
         var last_grp = '';
         var obj;
---
+++ locstredit_help.js
@@ -0,0 +1,80 @@
+lang = {};
+lang['^ko'] = {
+    title: '도움말 - 팩토리오 Locale 문자열 편집기',
+    1000: '도움말 - 팩토리오 Locale 문자열 편집기',
+    1010: '◆ 작업 과정',
+    1020: '1. 압축을 푼 팩토리오 모드의 <b>locale</b> 폴더 아래에 있는 풀버전인 <b>.cfg</b> 파일에서 텍스트를 복사하여 이 앱의 <b>"원본"</b> 영역에 붙여넣습니다.',
+    1030: '2. (선택사항) 동일한 모드 또는 번역 모드의 불완전한 <b>.cfg</b> 파일에서 텍스트를 복사하여 이 앱의 <b>"현지화"</b> 영역에 붙여넣습니다.',
+    1040: '3. <b>"누락 부분 생성"</b>을 눌러서 <b>"현지화"</b> 영역에 포함되지 않은 키를 필터링 합니다. 출력물은 <b>"비교"</b> 영역에서 볼 수 있습니다.',
+    1050: '4. <b>"비교"</b> 영역의 텍스트를 원하는 방식을 사용해서 번역합니다.',
+    1060: '5. (선택사항) 번역 소프트웨어를 사용하려면, <b>"줄 번호 표기"</b>를 누른 다음 <b>"키 이름 떼기"</b>를 눌러 번역할 필요가 없는 키 이름을 제외합니다. 원래 형식으로 돌아가려면 <b>"키 이름 붙이기"</b>를 누릅니다.',
+    1070: '6. <b>"현지화 병합"</b>을 누르면 <b>"비교"</b> 텍스트를 <b>"현지화"</b>에 병합하여 최종 결과가 <b>"비교"</b> 영역에 나타납니다.',
+    1080: '* 모든 과정은 <b>"원본"</b> 영역의 텍스트를 유지하면서 수행해야 합니다.',
+    1090: '* 저장 및 실행 취소 기능은 지원되지 않습니다. 텍스트를 유지하려면 서드파티 텍스트 편집기를 자주 사용하십시오.',
+    2000: '◆ 메뉴 정보',
+    2010: '이 도움말 페이지',
+    2020: '줄 바꿈',
+    2025: '긴 문장에 적용되는 자동 줄 바꿈을 켜거나 끕니다.',
+    2030: '누락 부분 생성',
+    2035: '<b>"현지화"</b>에는 없고 <b>"원본"</b>에는 있는 키를 가진 <b>"비교"</b> 텍스트를 생성합니다.',
+    2040: '차이 부분 생성',
+    2045: '누락된 키를 포함하여 <b>"원본"</b>과 동일하지 않은 값을 가진 <b>"현지화"</b> 텍스트로 <b>"비교"</b> 텍스트를 생성합니다.',
+    2050: '동일 부분 생성',
+    2055: '<b>"현지화"</b>와 <b>"원본"</b> 양쪽에서 정확히 동일한 키와 값을 가진 내용의 <b>"비교"</b> 텍스트를 생성합니다.',
+    2060: '줄 번호 표기',
+    2065: '모든 3가지 영역의 각 줄 앞에 줄 번호를 삽입합니다.',
+    2070: '줄 번호 제거',
+    2075: '<b>"줄 번호 표기"</b>를 되돌립니다.',
+    2080: '키 이름 떼기',
+    2085: '키 이름을 제거하고 <b>"비교"</b> 영역의 텍스트에 값만 남겨둡니다. 원본 키 이름을 기억하려면 <b>"줄 번호 표기"</b>가 필요합니다.',
+    2090: '키 이름 붙이기',
+    2095: '<b>"원본"</b> 영역의 데이터를 참조하여 <b>"키 이름 떼기"</b>로 처리된 텍스트에 키 이름을 다시 붙입니다.',
+    2100: '현지화 병합',
+    2105: '<b>"원본"</b> 영역의 데이터를 참조하여 <b>"비교"</b>를 <b>"현지화"</b>에 병합하여 최종 결과를 <b>"비교"</b>에 보여줍니다.'
+};
+lang['^en'] = {
+    title: 'Help - Factorio Locale String Editor',
+    1000: 'Help - Factorio Locale String Editor',
+    1010: '◆ Work Progress',
+    1020: '1. Copy texts from the full version of <b>.cfg</b> file under the <b>locale</b> folder of an Factorio mod extraction, and paste into <b>"original"</b> section of this app.',
+    1030: '2. (optional) Copy texts from the incomplete version of <b>.cfg</b> file of same mod or translating mod, and paste into <b>"localised"</b> section of this app.',
+    1040: '3. Press <b>"generate missing"</b> to filter keys not included in <b>"localised"</b> section. See <b>"compare"</b> section to see output.',
+    1050: '4. Translate texts in <b>"compare"</b> section in your way.',
+    1060: '5. (optional) To use translation software, press <b>"tag line number"</b> and then <b>"detach key names"</b> to exclude key names not to be translated. Press <b>"attach key names"</b> to go back to original format.',
+    1070: '6. Press <b>"merge with localised"</b> to merge <b>"compare"</b> text into <b>"localised"</b> text, your final result will be ready in <b>"compare"</b> section.',
+    1080: '* All progress need to be done while maintaining text in <b>"original"</b> section.',
+    1090: '* Saving and undo features are not supported. Frequently use 3rd party text editor to keep your texts.',
+    2000: '◆ Menu Information',
+    2010: 'This help page',
+    2020: 'word wrap',
+    2025: 'Turn on/off automatic line feed for long sentences.',
+    2030: 'generate missing',
+    2035: 'Generate <b>"compare"</b> text with keys not in <b>"localised"</b> but in <b>"original"</b>.',
+    2040: 'generate difference',
+    2045: 'Generate <b>"compare"</b> text with values in <b>"localised"</b> not same to <b>"original"</b>, including missing keys.',
+    2050: 'generate identical',
+    2055: 'Generate <b>"compare"</b> text with keys and values exactly same in both <b>"localised"</b> and <b>"original"</b>.',
+    2060: 'tag line numbers',
+    2065: 'Insert line number in front of each lines for all 3 sections.',
+    2070: 'untag line numbers',
+    2075: 'Revert <b>"tag line numbers"</b>.',
+    2080: 'detach key names',
+    2085: 'Remove key names and leave only values in text of <b>"compare"</b> section. <b>"tag line numbers"</b> is needed to remember original keys.',
+    2090: 'attach key names',
+    2095: 'Reattach key names to text processed by <b>"detach key names"</b>, refering data in <b>"original"</b> section.',
+    2100: 'merge with localised',
+    2105: 'Refering data in <b>"original"</b> section, merge <b>"localised"</b> with <b>"compare"</b> and put final result into <b>"compare"</b>.'
+};
+window.onload = function(){
+    var reg;
+    for(var key in lang){
+        reg = new RegExp(key, 'i');
+        if(navigator.language.match(reg)){
+            var doms = document.querySelectorAll('[data-langKey]');
+            for(var i = 0; i < doms.length; i++){
+                doms[i].innerHTML = lang[key][doms[i].dataset['langkey']]
+            };
+            break;
+        }
+    }
+};
